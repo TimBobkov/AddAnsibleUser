3be4c27573d666bf9ffff37bbe49245808b96098
Timofey Bobkov <bobkov.tim@gmail.com>
2017-10-13 21:37:48 +0300

    debugging

--- instal.sh
+++ instal.sh
@@ -12,8 +12,8 @@ pip install docker-compose==1.15.0
 
 #add and cofig user
 useradd -m -s /bin/bash ansible #создали пользователя
-password=$(head -c 100 /dev/urandom | base64 | sed 's/[+=/A-Z]//g' | tail -c 11) #сгенерировали пароль
-echo -e "$password\n$password\n" | passwd ansible #изменили пароль
+#password=$(head -c 100 /dev/urandom | base64 | sed 's/[+=/A-Z]//g' | tail -c 11) #сгенерировали пароль
+#echo -e "$password\n$password\n" | passwd ansible #изменили пароль
 mkdir -p /home/ansible/.ssh #создали папку под ключи
 ssh-keygen -i -f ./keys/key.pub >> /home/ansible/.ssh/authorized_keys #добавили ключ для входа
 chown -R ansible:ansible /home/ansible/.ssh/ #изменили владельца папки на пользователя ansible
