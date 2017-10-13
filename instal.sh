bbc9ef9bc3c8bb8f5b56ffd3c7dccb456778b917
Timofey Bobkov <bobkov.tim@gmail.com>
2017-10-13 21:37:03 +0300

    debugging

--- instal.sh
+++ instal.sh
@@ -15,12 +15,13 @@ useradd -m -s /bin/bash ansible #создали пользователя
 password=$(head -c 100 /dev/urandom | base64 | sed 's/[+=/A-Z]//g' | tail -c 11) #сгенерировали пароль
 echo -e "$password\n$password\n" | passwd ansible #изменили пароль
 mkdir -p /home/ansible/.ssh #создали папку под ключи
-ssh-keygen -i -f ./keys/id_rsa.pub >> /home/ansible/.ssh/authorized_keys #добавили ключ для входа
+ssh-keygen -i -f ./keys/key.pub >> /home/ansible/.ssh/authorized_keys #добавили ключ для входа
 chown -R ansible:ansible /home/ansible/.ssh/ #изменили владельца папки на пользователя ansible
 chmod 700 -R /home/ansible/.ssh/ #дали правильные права
 groupadd docker #добавили группу для docker, если ее нет
 usermod -a -G docker ansible #добавили пользователя в группу
 service docker restart #перезапустили docker
+usermod -a -G sudo ansible
 echo "ansible ALL=(ALL:ALL) NOPASSWD: /usr/bin/curl -L https\://github.com/docker/compose/releases/download/*/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose" | EDITOR='tee -a' visudo #даем право пользователю ansible на обновление docker-compose
 echo "ansible ALL=(ALL:ALL) NOPASSWD: /usr/bin/apt-get install -y --only-upgrade docker-ce" | EDITOR='tee -a' visudo #даем право пользователю ansible на обновление docker
 echo "ansible ALL=(ALL:ALL) NOPASSWD: /usr/bin/apt-get update" | EDITOR='tee -a' visudo #даем право пользователю ansible на обновление локального кеша репозиториев
