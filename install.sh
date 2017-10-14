46e9e00e293e1fda47b8b801f01370be12aa6abd
Timofey Bobkov <bobkov.tim@gmail.com>
2017-10-14 17:58:27 +0300

    debugging

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-#install prerequisites
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y apt-transport-https ca-certificates docker-ce python-pip
-curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-pip install docker
-pip install docker-compose==1.15.0
-
 #add and cofig user
 useradd -m -s /bin/bash ansible #создали пользователя
 #password=$(head -c 100 /dev/urandom | base64 | sed 's/[+=/A-Z]//g' | tail -c 11) #сгенерировали пароль
